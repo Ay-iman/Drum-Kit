3c64e18360d9b959e91c7a5230c7ccd622ff0a55
Ay-iman <ayomiderabiu00@gmail.com>
2023-04-15 15:39:11 +0100

    Drum Kit

---
+++ index.js
@@ -0,0 +1,57 @@
+var numberOfDrums = document.querySelectorAll(".drum");
+
+// Button
+function whenClick() {
+  var buttonInnerHtml = this.innerHTML;
+  makeSound(buttonInnerHtml)
+}
+
+
+function makeSound(key) {
+    switch (key) {
+      case "w":
+        var audio = new Audio("./sounds/tom-1.mp3");
+        audio.play();
+        break;
+
+      case "a":
+        var audio = new Audio("./sounds/tom-2.mp3");
+        audio.play();
+        break;
+
+      case "s":
+        var audio = new Audio("./sounds/tom-3.mp3");
+        audio.play();
+        break;
+
+      case "d":
+        var audio = new Audio("./sounds/tom-4.mp3");
+        audio.play();
+        break;
+
+      case "j":
+        var audio = new Audio("./sounds/crash.mp3");
+        audio.play();
+        break;
+
+      case "k":
+        var audio = new Audio("./sounds/snare.mp3");
+        audio.play();
+        break;
+
+      case "l":
+        var audio = new Audio("./sounds/kick-bass.mp3");
+        audio.play();
+        break;
+
+      default:
+        break;
+    }
+}
+
+for (var i = 0; i < numberOfDrums.length; i++) {
+  numberOfDrums[i].addEventListener("click", whenClick);
+  numberOfDrums[i].addEventListener("keypress", function (event) { //keyboard 
+    makeSound(event.key)
+  });
+}
